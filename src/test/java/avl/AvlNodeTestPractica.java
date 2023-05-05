550d63986495fb55f832cfcddae37ac3c89412a7
DESKTOP-TAADH65\josem <josemanuelsannav@uma.es>
2023-05-05 12:18:12 +0200

    dnbdvb

--- src/test/java/avl/AvlNodeTestPractica.java
+++ src/test/java/avl/AvlNodeTestPractica.java
@@ -1,4 +1,18 @@
 package avl;
 
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
 public class AvlNodeTestPractica {
+    @Test
+    @DisplayName("Test para el constructor al pasarle cualquier objeto valido")
+    void test1(){
+
+    }
+
+    @Test
+    @DisplayName("Test para el constructor al pasarle un objeto null")
+    void test2(){
+
+    }
 }
