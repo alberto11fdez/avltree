56083434e3909374ffe4c2b91a5497f7688eddee
DESKTOP-8IF5MAA\USUARIO <alberto11fdez@gmail.com>
2023-05-12 12:00:27 +0200

    Node hecho

--- src/test/java/avl/AvlNodePracticaTest.java
+++ src/test/java/avl/AvlNodePracticaTest.java
@@ -3,9 +3,9 @@ package avl;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.support.hierarchical.Node;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AvlNodePracticaTest {
     @Test
@@ -42,28 +42,68 @@ public class AvlNodePracticaTest {
         assertThrows(IllegalArgumentException.class, () -> {AvlNode<Object> nodoNull = new AvlNode<>(null);});
 
     }
-/*
+
     @Test
-    @DisplayName("Test para comprobar que se puede insertar un nodo")
+    @DisplayName("Test para comprobar que un nodo completo, con padre, hijoDrcho, HijoIzq, NodoCercano y con peso devuelve el item del nodo")
     public void test3(){
         AvlNode<Object>Node1 = new AvlNode<>(1);
 
+        Node1.setHeight(1);
+        Node1.setLeft(new AvlNode<>(2));
+        Node1.setRight(new AvlNode<>(3));
+        Node1.setParent(new AvlNode<>(0));
+        Node1.setClosestNode(new AvlNode<>(4));
+        Node1.updateHeight();
+        Node1.setItem(1);
+
+        assertEquals(1, Node1.getItem());
+        assertTrue(Node1.hasParent());
+        assertTrue(Node1.hasRight());
+        assertTrue(Node1.hasLeft());
     }
 
     @Test
-    @DisplayName("Test para comprobar que se puede buscar un nodo insertado")
+    @DisplayName("Test para comprobar que un nodo solo tiene hijo izquierdo")
     public void test4(){
+        AvlNode<Object>Node1 = new AvlNode<>(1);
 
+        Node1.setLeft(new AvlNode<>(2));
+
+        Node1.updateHeight();
+        assertFalse(Node1.isLeaf());
+        assertFalse(Node1.hasOnlyARightChild());
+        assertTrue(Node1.hasOnlyALeftChild());
     }
 
 
     @Test
-    @DisplayName("Test para comprobar que se puede borrar un nodo buscado")
+    @DisplayName("Test para comprobar que un nodo solo tiene hijo derecho")
     public void test5(){
+        AvlNode<Object>Node1 = new AvlNode<>(1);
 
+        Node1.setRight(new AvlNode<>(2));
+
+        Node1.updateHeight();
+
+
+        assertFalse(Node1.isLeaf());
+        assertFalse(Node1.hasOnlyALeftChild());
+        assertTrue(Node1.hasOnlyARightChild());
     }
 
- */
+    @Test
+    @DisplayName("Test para comprobar que un nodo es una hoja")
+    public void test6(){
+        AvlNode<Object>Node1 = new AvlNode<>(1);
+        Node1.updateHeight();
+
+        assertTrue(Node1.isLeaf());
+        assertFalse(Node1.hasParent());
+        assertFalse(Node1.hasOnlyALeftChild());
+        assertFalse(Node1.hasOnlyARightChild());
+    }
+
+
 
 
 }
